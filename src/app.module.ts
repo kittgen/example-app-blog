878c8e2ad2bceaa59049e560b384531feb3241b3
Otto von Wesendonk <ottovonwesendonk@gmail.com>
2020-12-22 19:04:13 +0100

    initial resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticlesModule } from './articles/articles.module';
+import { TagsModule } from './tags/tags.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { PermissionsModule } from './permissions/permissions.module';
 
 @Module({
-  imports: [],
+  imports: [ArticlesModule, TagsModule, UsersModule, RolesModule, PermissionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
